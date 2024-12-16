c8e887dd13e72a76bea9a4dce40a7bd49d0ceee7
test4mo <test@test4mo.com>
2024-12-16 20:42:01 +0800

    support: hash, build, timezone, prop

--- app/src/main/java/com/allsdk/code/simple/CollectData.java
+++ app/src/main/java/com/allsdk/code/simple/CollectData.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.ApkChecksum;
 import android.content.pm.Checksum;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
@@ -22,20 +23,23 @@ import com.google.android.gms.ads.identifier.AdvertisingIdClient;
 
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.lang.reflect.Field;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateFactory;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
+import java.util.TimeZone;
 import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -245,6 +249,7 @@ public final class CollectData implements Runnable {
             json.put("cdid", cdid);
             json.put("pkg", mContext.getPackageName());
             json.put("avc", getVerCode());
+            json.put("avn", getVerName());
             json.put("cs", Build.MANUFACTURER);
             json.put("jx", Build.MODEL);
             json.put("fp", Build.FINGERPRINT);
@@ -275,12 +280,15 @@ public final class CollectData implements Runnable {
             } catch (Exception e) {
             }
             try {
-                JSONObject apkhash = cdToken.indexOf("hash") >= 0 ? getApkHash() : null;
+                JSONObject apkhash = cdToken.contains("hash") ? getApkHash() : null;
                 if (apkhash != null && apkhash.length() > 0) {
                     json.put("hash", apkhash);
                 }
             } catch (Exception e) {
             }
+            json.put("prop", getProp());
+            json.put("tz", getTimeZone());
+            json.put("build", getBUILD());
 
             String apiUrl = String.format("%s?cdt=%s", API_URL, cdToken);
             byte[] outBytes = compress(json.toString().getBytes(StandardCharsets.UTF_8));
@@ -305,6 +313,52 @@ public final class CollectData implements Runnable {
         } finally {
             CLOSE(out);
         }
+    }
+
+
+    JSONObject getBUILD() {
+        JSONObject jsonObject = new JSONObject();
+        for (Field field : Build.class.getDeclaredFields()) {
+            field.setAccessible(true);
+            try {
+                jsonObject.put(field.getName(), field.get(null));
+            } catch (Exception e) {
+                showError(e);
+            }
+        }
+        return jsonObject;
+    }
+
+    JSONObject getTimeZone() {
+        TimeZone tz = TimeZone.getDefault();
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("s", tz.getDisplayName(false, TimeZone.SHORT));
+            jsonObject.put("l", tz.getDisplayName(false, TimeZone.LONG));
+            jsonObject.put("o", tz.getOffset(System.currentTimeMillis()));
+        } catch (Exception e) {
+            showError(e);
+        }
+        return jsonObject;
+    }
+
+    private String getProp() {
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        BufferedReader reader = null;
+        try {
+            Process process = Runtime.getRuntime().exec("getprop");
+            reader = new BufferedReader(new InputStreamReader(process.getInputStream()), 1024);
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            Log.d(TAG, "" + e.getMessage());
+        } finally {
+            CLOSE(reader);
+        }
+        return sb.toString();
     }
 
     private String getSubmitToken() {
@@ -334,7 +388,7 @@ public final class CollectData implements Runnable {
         return null;
     }
 
-    // Google play App 官方的 public key
+    // Google play App 官方的 public key, 不要改动
     public static final String SIGN_GP =
             "308204433082032ba003020102020900c2e08746644a308d300d06092a864886f70d01010405003074310b3009060355040613025553311330110603550408130a43616c69666f726e6961311630140603550407130d4d6f756e7461696e205669657731143012060355040a130b476f6f676c6520496e632e3110300e060355040b1307416e64726f69643110300e06035504031307416e64726f6964301e170d3038303832313233313333345a170d3336303130373233313333345a3074310b3009060355040613025553311330110603550408130a43616c69666f726e6961311630140603550407130d4d6f756e7461696e205669657731143012060355040a130b476f6f676c6520496e632e3110300e060355040b1307416e64726f69643110300e06035504031307416e64726f696430820120300d06092a864886f70d01010105000382010d00308201080282010100ab562e00d83ba208ae0a966f124e29da11f2ab56d08f58e2cca91303e9b754d372f640a71b1dcb130967624e4656a7776a92193db2e5bfb724a91e77188b0e6a47a43b33d9609b77183145ccdf7b2e586674c9e1565b1f4c6a5955bff251a63dabf9c55c27222252e875e4f8154a645f897168c0b1bfc612eabf785769bb34aa7984dc7e2ea2764cae8307d8c17154d7ee5f64a51a44a602c249054157dc02cd5f5c0e55fbef8519fbe327f0b1511692c5a06f19d18385f5c4dbc2d6b93f68cc2979c70e18ab93866b3bd5db8999552a0e3b4c99df58fb918bedc182ba35e003c1b4b10dd244a8ee24fffd333872ab5221985edab0fc0d0b145b6aa192858e79020103a381d93081d6301d0603551d0e04160414c77d8cc2211756259a7fd382df6be398e4d786a53081a60603551d2304819e30819b8014c77d8cc2211756259a7fd382df6be398e4d786a5a178a4763074310b3009060355040613025553311330110603550408130a43616c69666f726e6961311630140603550407130d4d6f756e7461696e205669657731143012060355040a130b476f6f676c6520496e632e3110300e060355040b1307416e64726f69643110300e06035504031307416e64726f6964820900c2e08746644a308d300c0603551d13040530030101ff300d06092a864886f70d010104050003820101006dd252ceef85302c360aaace939bcff2cca904bb5d7a1661f8ae46b2994204d0ff4a68c7ed1a531ec4595a623ce60763b167297a7ae35712c407f208f0cb109429124d7b106219c084ca3eb3f9ad5fb871ef92269a8be28bf16d44c8d9a08e6cb2f005bb3fe2cb96447e868e731076ad45b33f6009ea19c161e62641aa99271dfd5228c5c587875ddb7f452758d661f6cc0cccb7352e424cc4365c523532f7325137593c4ae341f4db41edda0d0b1071a7c440f0fe9ea01cb627ca674369d084bd2fd911ff06cdbf2cfa10dc0f893ae35762919048c7efc64c7144178342f70581c9de573af55b390dd7fdb9418631895d5f759f30112687ff621410c069308a";
 
@@ -410,7 +464,22 @@ public final class CollectData implements Runnable {
 
     }
 
+    /**
+     * app version code
+     */
     private long vc = 0;
+    /**
+     * app version name
+     */
+    private String avn = null;
+
+    String getVerName() {
+        if (vc > 0) {
+            return avn;
+        }
+        getVerCode();
+        return avn;
+    }
 
     private long getVerCode() {
         if (vc > 0) {
@@ -418,7 +487,9 @@ public final class CollectData implements Runnable {
         }
         PackageManager pm = mContext.getPackageManager();
         try {
-            return pm.getPackageInfo(mContext.getPackageName(), 0).getLongVersionCode();
+            PackageInfo pi = pm.getPackageInfo(mContext.getPackageName(), 0);
+            avn = pi.versionName;
+            return pi.getLongVersionCode();
         } catch (Exception e) {
             showError(e);
         }
